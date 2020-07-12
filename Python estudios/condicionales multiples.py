9e1f688357452458663d109ddebe7327fec4048d
=Angie <=agonzalez@suyo.co>
2020-07-12 16:49:26 -0500

    Convertidor numeros a letras

---
+++ Python estudios/condicionales multiples.py
@@ -0,0 +1,24 @@
+print ("¡Convertidor de numeros a letras!!")
+
+numero = int(input("¿Cual es el numero que deseas convertir?:"))
+
+
+if numero == 1:
+    print ("El numero es uno.")
+
+elif numero == 2:
+    print ("El numero es Dos.")
+
+elif numero == 3:
+    print ("El numero es Tres.")
+
+elif numero == 4:
+    print ("El numero es Cuatro.")
+
+elif numero == 5:
+    print ("El numero es Cinco.")
+    
+else:
+    print ("Solo es permitido mostrar hasta el numero 5!.")
+
+print ("Fin.")
